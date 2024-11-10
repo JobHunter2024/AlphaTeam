c4f444c5eb069ab5e97ed7b97f654d12fce27d31
Bianca <143421359+olarubianca6@users.noreply.github.com>
2024-11-10 12:38:48 +0200

    AOP and BPMN

--- Olaru Bianca/TaskManager/src/main/java/com/example/task/factory/TaskFactory.java
+++ Olaru Bianca/TaskManager/src/main/java/com/example/task/factory/TaskFactory.java
@@ -1,27 +1,27 @@
-//package com.example.task.factory;
-//
-//import com.example.task.processor.TaskConfig;
-//import com.example.task.queue.TaskQueueManager;
-//
-//public class TaskFactory {
-//    private final TaskQueueManager taskQueueManager;
-//
-//    public TaskFactory(TaskQueueManager taskQueueManager) {
-//        this.taskQueueManager = taskQueueManager;
-//    }
-//
-//    public ScrapeTaskCommand createTask(TaskConfig taskConfig) {
-//        ScrapeTaskCommand taskCommand;
-//
-//        switch (taskConfig.getType()) {
-//            case "scrape":
-//                taskCommand = new ScrapeTaskCommand(taskConfig);
-//                break;
-//            default:
-//                throw new IllegalArgumentException("Task type does not exist: " + taskConfig.getType());
-//        }
-//
-//        taskQueueManager.addToQueue(taskCommand);
-//        return taskCommand;
-//    }
-//}
+package com.example.task.factory;
+
+import com.example.task.processor.TaskConfig;
+import com.example.task.queue.TaskQueueManager;
+
+public class TaskFactory {
+    private final TaskQueueManager taskQueueManager;
+
+    public TaskFactory(TaskQueueManager taskQueueManager) {
+        this.taskQueueManager = taskQueueManager;
+    }
+
+    public ScrapeTaskCommand createTask(TaskConfig taskConfig) {
+        ScrapeTaskCommand taskCommand;
+
+        switch (taskConfig.getType()) {
+            case "scrape":
+                taskCommand = new ScrapeTaskCommand(taskConfig);
+                break;
+            default:
+                throw new IllegalArgumentException("Task type does not exist: " + taskConfig.getType());
+        }
+
+        taskQueueManager.addToQueue(taskCommand);
+        return taskCommand;
+    }
+}
--- Olaru Bianca/TaskManager/src/main/java/com/example/task/queue/TaskDispatcher.java
+++ Olaru Bianca/TaskManager/src/main/java/com/example/task/queue/TaskDispatcher.java
@@ -1,25 +1,25 @@
-//package com.example.task.queue;
-//
-//import com.example.task.MockFetchingService;
-//import com.example.task.factory.TaskCommand;
-//import com.example.task.processor.TaskProcessor;
-//
-//public class TaskDispatcher {
-//    private final TaskQueueManager taskQueueManager;
-//    private final MockFetchingService fetchingService;
-//
-//    public TaskDispatcher(TaskQueueManager taskQueueManager, MockFetchingService fetchingService, TaskProcessor taskProcessor) {
-//        this.taskQueueManager = taskQueueManager;
-//        this.fetchingService = fetchingService;
-//    }
-//
-//    public void dispatch() {
-//        while (!taskQueueManager.isQueueEmpty()) {
-//            TaskCommand task = taskQueueManager.getFromQueue();
-//            if (task != null) {
-//                fetchingService.fetchData(task.toServiceRequest());
-//                System.out.println("Dispatched task: " + task);
-//            }
-//        }
-//    }
-//}
+package com.example.task.queue;
+
+import com.example.task.MockFetchingService;
+import com.example.task.factory.TaskCommand;
+import com.example.task.processor.TaskProcessor;
+
+public class TaskDispatcher {
+    private final TaskQueueManager taskQueueManager;
+    private final MockFetchingService fetchingService;
+
+    public TaskDispatcher(TaskQueueManager taskQueueManager, MockFetchingService fetchingService, TaskProcessor taskProcessor) {
+        this.taskQueueManager = taskQueueManager;
+        this.fetchingService = fetchingService;
+    }
+
+    public void dispatch() {
+        while (!taskQueueManager.isQueueEmpty()) {
+            TaskCommand task = taskQueueManager.getFromQueue();
+            if (task != null) {
+                fetchingService.fetchData(task.toServiceRequest());
+                System.out.println("Dispatched task: " + task);
+            }
+        }
+    }
+}
